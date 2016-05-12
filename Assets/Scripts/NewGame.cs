6c2b80ae8d1643ee19523c0e85fe294d0be3c442
Chris Apczynski <capczyns@umich.edu>
2016-05-12 01:59:08 -0400

    Add Main Menu
    Actually David's commit

---
+++ Assets/Scripts/NewGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NewGame : MonoBehaviour {
+
+
+	public void StartNewGame (int SceneToChangeTo) 
+	{
+		SceneManager.LoadScene (SceneToChangeTo);
+	}
+	
+
+}
