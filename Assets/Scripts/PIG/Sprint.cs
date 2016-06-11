60a8acb5a49cd8fab4f334150b1491d2ccd5239a
Chris Apczynski <capczyns@umich.edu>
2016-06-11 03:17:53 -0400

    Add task display

--- Assets/Scripts/PIG/Sprint.cs
+++ Assets/Scripts/PIG/Sprint.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Sprint : MonoBehaviour {
 
@@ -10,6 +11,8 @@ public class Sprint : MonoBehaviour {
 	public int sprintDuration = 0;
 	public int currentDay = 0;
 	public int budget = 5000;
+
+	public Task[] tasks;
 
 	public int updateLinesObjective(int change){
 		linesObjective += change;
@@ -37,7 +40,30 @@ public class Sprint : MonoBehaviour {
 	}
 	// Use this for initialization
 	void Start () {
-	
+		tasks = new Task[4];
+
+		GameObject taskHolder = GameObject.Find ("TaskHolder");
+		float holderWidth = taskHolder.GetComponent<RectTransform> ().rect.width;
+
+		//	This first loop fills in 4 tasks for our test run, it should be removed eventually
+		for (int index = 0; index < tasks.Length; ++index) {
+			tasks [index] = new Task (500, 13, "Task " + (index + 1));
+		}
+
+		//	This second loop figures out lines based on tasks and whatnot, also creates the task identifiers
+		for (int index = 0; index < tasks.Length; ++index) {
+			linesObjective += tasks [index].lines;
+			defectLimit += tasks [index].allowedDefects;
+		}
+
+		float taskSizer = holderWidth / linesObjective;
+		for (int index = 0; index < tasks.Length; ++index) {
+			GameObject task = (GameObject)Instantiate (Resources.Load ("UIElements/Task"));
+			task.transform.GetChild (0).GetComponent<Text> ().text = tasks [index].name;
+			task.GetComponent<LayoutElement> ().preferredWidth = tasks [index].lines * taskSizer;
+			task.transform.SetParent (taskHolder.transform);
+			task.transform.localScale = new Vector3 (1, 1, 1);
+		}
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/PIG/Task.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Task {
+
+	public int lines = 500;
+	public int allowedDefects = 25;
+	public string name = "name";
+	public Task(int lines, int allowedDefects, string name){
+		this.lines = lines;
+		this.allowedDefects = allowedDefects;
+		this.name = name;
+	}
+	public Task(){
+		lines = 500;
+		allowedDefects = 13;
+		name = "name";
+	}
+	// Use this for initialization
+}
