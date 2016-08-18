e36804ba76cbc5178eb9c44f35ed1edd86469c93
Chris Apczynski <capczyns@umich.edu>
2016-08-18 05:01:52 -0400

    Add check for tasks complete while sprints left to end project

--- Assets/Scripts/PIG/SummaryFields/CloseButton.cs
+++ Assets/Scripts/PIG/SummaryFields/CloseButton.cs
@@ -11,6 +11,14 @@ public class CloseButton : MonoBehaviour {
 		PersistantData.persistantData.projects [PersistantData.persistantData.projectIndex].sprintsDone+= 1;
 		sprintsLeft = PersistantData.persistantData.projects [PersistantData.persistantData.projectIndex].totalSprints -
 			PersistantData.persistantData.projects [PersistantData.persistantData.projectIndex].sprintsDone;
+		bool incompleteFound = false;
+		foreach (Task task in PersistantData.persistantData.projects[PersistantData.persistantData.projectIndex].tasks) {
+			incompleteFound = task.linesDone >= task.lines;
+			if (incompleteFound) {
+				sprintsLeft = 0;
+				break;
+			}
+		}
 		if (sprintsLeft > 0) {
 			SceneManager.LoadScene ("SprintBacklogSelection");
 		} else {
