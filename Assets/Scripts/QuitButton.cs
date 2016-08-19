7695dae19038eefb198312ba0561c31d43be508f
Ethan Evans <ewevans@umich.edu>
2016-08-19 08:05:46 -0400

    fixed Exit button and put scripts in folder

---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QuitButton : MonoBehaviour {
+
+	Button button;
+	// Use this for initialization
+	void Awake () {
+		button = GetComponent<Button> ();
+
+
+		button.onClick.AddListener (() => {
+			whenClicked ();
+		});
+	}
+
+	void whenClicked(){
+		Application.Quit ();
+
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+	}
+
+
+
+}
+
+
+
+
+
+
