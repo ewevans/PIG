48d45a0859fdf2554538b616c905a34112bd9d50
Chris Apczynski <capczyns@umich.edu>
2016-08-20 06:06:57 -0400

    Add Project Name and Sprint Number to Sprint Summary

---
+++ Assets/Scripts/PIG/SummaryFields/SSProjectName.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SSProjectName : MonoBehaviour {
+	public GameObject gameSystem;
+
+	// Use this for initialization
+	void Start () {
+		Project project = PersistantData.persistantData.projects [PersistantData.persistantData.projectIndex];
+		GetComponent<Text>().text = project.name + " Sprint " + (project.sprintsDone + 1) + " Summary:";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
